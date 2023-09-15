2e915c95131274b3d4763a9c48378f2c2b49b6df
wlbimalutym <wlbimalutym195@gmail.com>
2023-09-15 17:12:31 +0300

    adaptive-tablet add

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-function readMore() {
+function readMoreMobile() {
     var more = document.getElementById('more');
-    var btn = document.getElementById('more-btn');
+    var btn = document.getElementById('more-btn-mobile');
 
     if(more.style.display === 'none') {
         more.style.display = 'inline';
@@ -8,6 +8,18 @@ function readMore() {
     }
     else {
         more.style.display = 'none';
-		btn.innerHTML = 'Читать далее'
+		btn.innerHTML = 'Читать далее';
     }
+}
+function readMoreTablet() {
+	var moreTablet = document.getElementById('more-second-half')
+	var btnTablet = document.getElementById('more-btn--tablet');
+
+	if (moreTablet.style.display === 'none') {
+		moreTablet.style.display = 'inline';
+		btnTablet.innerHTML = 'Скрыть';
+	} else {
+		moreTablet.style.display = 'none';
+		btnTablet.innerHTML = 'Читать далее';
+	}
 }
