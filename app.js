605c695f368cdd813af43d6bad56567c0f0316bd
wlbimalutym <wlbimalutym195@gmail.com>
2023-10-10 18:26:23 +0300

    npm swiper

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	loop: true,
+
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+})
+
 function readMoreMobile() {
     var more = document.getElementById('more');
     var btn = document.getElementById('more-btn-mobile');
